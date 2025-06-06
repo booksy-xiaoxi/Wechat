558dbbef14784e6ed905b41d24ce8df520fa9544
booksy-xiaoxi <xiaoxi359349@qq.com>
2025-06-06 21:02:38 +0800

    feat: 添加语音播放按钮及相关音频文件

--- pages/second/second.js
+++ pages/second/second.js
@@ -5,21 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    status: 'warning' // 可为 'safe', 'danger', 'warning'
+    status: 'warning',// 可为 'safe', 'danger', 'warning'
+    audioUrl: 'https://yourserver.com/audio/example.mp3' // 替换为你的音频链接
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
   },
 
   /**
@@ -37,34 +36,42 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide() {
-
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage() {
+  },
+  playAudio() {
+    const innerAudioContext = wx.createInnerAudioContext();
+    innerAudioContext.src = this.data.audioUrl;
+    innerAudioContext.play();
 
+    innerAudioContext.onPlay(() => {
+      console.log('开始播放');
+    });
+
+    innerAudioContext.onError((res) => {
+      console.error('播放出错', res.errMsg);
+    });
   }
 })
